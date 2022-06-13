f5bdf2c9c5733eac665745a5beb7ad0d9ed30ef3
Balajee Mishra <bala.44472@gmail.com>
2022-06-13 15:43:47 +0530

    done

--- models/transaction.js
+++ models/transaction.js
@@ -1,3 +1,14 @@
 const mongoose = require("mongoose");
-const TransactionSchema = new mongoose.Schema({});
+const TransactionSchema = new mongoose.Schema(
+  {
+    amount: {
+      type: Number,
+    },
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
 module.exports = mongoose.model("TransactionDetail", TransactionSchema);
